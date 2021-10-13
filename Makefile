jbrubake/jmines

commit 20f2b125659ed38ebdbab5575ced9575cd393620

Add cscope target to Makefile and adjust ctags target

Makefile
@@ -1,6 +1,7 @@
 CC=gcc
 LINT=splint
 CTAGS=ctags
+CSCOPE=cscope
 RM=rm
 MKDEPEND=$(CC) -MM
 DOXYGEN=doxygen
@@ -9,12 +10,14 @@ INCLUDES=
 CFLAGS=-c -Wall -std=c99 $(INCLUDES) -DDEBUG
 LDFLAGS=
 
-SOURCES=jmines.c game.c cli.c
+OURCES=jmines.c game.c cli.c
 OBJECTS=$(SOURCES:.c=.o)
 HEADERS=$(SOURCES:.c=.h) version.h
 EXE=
 PROGNAME=jmines$(EXE)
 COREFILE=
+
+TAGFILE=.git/tags
 
 all: $(SOURCES) $(PROGNAME)
 
@@ -28,7 +31,10 @@ depend: $(SOURCES)
 	$(MKDEPEND) $(SOURCES) > .depend
 
 TAGS:
-	$(CTAGS) *
+	$(CTAGS) -f $(TAGFILE) -R *
+
+cscope:
+	$(CSCOPE) -R -b
 
 lint: $(SOURCES) $(HEADERS)
 	$(LINT) $(SOURCES)

commit a7226f152b552ff1e55cbc0df4e7e8960a67fafd

Fix typo introduced in 20f2b125659

Makefile
@@ -10,7 +10,7 @@ INCLUDES=
 CFLAGS=-c -Wall -std=c99 $(INCLUDES) -DDEBUG
 LDFLAGS=
 
-OURCES=jmines.c game.c cli.c
+SOURCES=jmines.c game.c cli.c
 OBJECTS=$(SOURCES:.c=.o)
 HEADERS=$(SOURCES:.c=.h) version.h
 EXE=